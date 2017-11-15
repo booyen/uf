3b03704d8c819c1f9819fdd6a5342a83022a3c02
Gxcc <gxcc@github.com>
2017-11-15 20:12:11 +0800

    Change Deprecated Code
    Older version of Mysql code deprecate, CHnage to new one

--- general_base/frontend_search.php
+++ general_base/frontend_search.php
@@ -4,18 +4,12 @@
 <?php require '../inc/views/template_head_end.php'; ?>
 <?php require '../inc/views/base_header_dashboard_frontend.php'; ?>
 
+
 <?php
-    mysql_connect("localhost", "root", "") or die("Error connecting to database: ".mysql_error());
-    /*
-        localhost - it's location of the mysql server, usually localhost
-        root - your username
-        third is your password
-         
-        if connection fails it will stop loading the page and display an error
-    */
+   $conn=mysqli_connect("localhost", "root", "") or die("Error connecting to database: ".mysql_error());
+
      
-    mysql_select_db("ummah") or die(mysql_error());
-    /* tutorial_search is the name of database we've created */
+    mysqli_select_db($conn,"ummah") or die(mysql_error());
 ?>
 
 
@@ -81,10 +75,10 @@
         $query = htmlspecialchars($query); 
         // changes characters used in html to their equivalents, for example: < to &gt;
          
-        $query = mysql_real_escape_string($query);
+        $query = mysqli_real_escape_string($conn,$query);
         // makes sure nobody uses SQL injection
          
-        $raw_results = mysql_query("SELECT * FROM org_profile
+        $raw_results = mysqli_query($conn,"SELECT * FROM org_profile
             WHERE (`org_fullname` LIKE '%".$query."%') OR
             (`org_username` LIKE '%".$query."%')  
             ") or die(mysql_error());
@@ -120,9 +114,9 @@
                         </tr>
                     </thead>
                     <?php
-                         if(mysql_num_rows($raw_results) > 0){ // if one or more rows are returned do following
+                         if(mysqli_num_rows($raw_results) > 0){ // if one or more rows are returned do following
              
-            while($results = mysql_fetch_array($raw_results)){
+            while($results = mysqli_fetch_array($raw_results)){
             // $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
              
                 $orgid = $results["org_proid"];
